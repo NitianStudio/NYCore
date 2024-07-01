276595eaaad8affda92fed30d8bd21d56de5e328
baka4n <qq474899581@gmail.com>
2024-07-01 21:33:36 +0800

    commit init

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,24 @@ dependencies {
     implementation("net.minestom:minestom-snapshots:77af815afe")
 }
 
-tasks.test {
-    useJUnitPlatform()
+val targetJavaVersion = 21
+
+java {
+    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+    sourceCompatibility = javaVersion
+    targetCompatibility = javaVersion
+    if (JavaVersion.current() < javaVersion) {
+        toolchain.languageVersion = JavaLanguageVersion.of(targetJavaVersion)
+    }
+}
+
+tasks {
+    compileJava {
+        if (targetJavaVersion >= 10 || JavaVersion.current().isJava10Compatible) {
+            options.release = targetJavaVersion
+        }
+    }
+    javadoc {
+        options.encoding = Charsets.UTF_8.name()
+    }
 }
